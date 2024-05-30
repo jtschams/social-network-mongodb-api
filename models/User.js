3dcd553c2641117748d80d6bc940c14a2e464468
jtschams <jtschams@yahoo.com>
2024-05-30 16:00:21 -0500

    added model index and user model

--- models/User.js
+++ models/User.js
@@ -1,24 +1,40 @@
-// TODO
-// User:
+const { Schema, model } = require('mongoose');
 
-// username
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim:true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/
+    },
+    thoughts: [{
+      type: Schema.Types.ObjectId,
+      ref: 'Thought'
+    }],
+    friends: [{
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }]
+  },
+  {
+    toJSON: {
+      virtuals: true
+    }
+  }
+);
 
-  // String
-  // Unique
-  // Required
-  // Trimmed
-// email
+userSchema.virtual(friendCount)
+  .get(function() {
+    return this.friends.length
+  });
 
-  // String
-  // Required
-  // Unique
-  // Must match a valid email address (look into Mongoose's matching validation)
-// thoughts
+  const User = model('User', userSchema);
 
-  // Array of _id values referencing the Thought model
-// friends
-
-  // Array of _id values referencing the User model (self-reference)
-// Schema Settings:
-
-// Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+  module.exports = User
